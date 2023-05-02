14756a544da74449b70e0a00e144e4033c902c1f
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-05-02 13:24:20 +0300

    Function

---
+++ index.js
@@ -0,0 +1,14 @@
+// single-line coment
+
+/*
+Multi
+line
+comment
+*/
+
+// Baziniu konceptu:
+// 1) kintamieji
+// 2) salygos (if)
+// 3) ciklai (for)
+// 4) funkcijos
+// 5) events (narsykles puseje)
---
+++ js/01-console.js
@@ -0,0 +1,20 @@
+console.clear(); //isvalo console
+/*
+CONSOLE - spausdiname norima rezultata/-us
+*/
+
+console.log(1);
+console.log(2);
+console.log(3, 4, 5);
+
+console.warn(1); 
+console.warn(2);
+console.warn(3, 4, 5);
+
+console.error(1);
+console.error(2);
+console.error(3, 4, 5);
+
+console.log(45)
+console.warn(3, 45, 85)
+console.error(25, 45, 45)
