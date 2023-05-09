50f3f8ce1a1f92ebc17e4424906ef5e8366cf835
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-05-09 16:07:57 +0300

    Funkcija

---
+++ uzd/kintamieji.js
@@ -0,0 +1,51 @@
+console.clear();
+
+const number = 1;
+console.log(number);
+
+const numb = 2;
+console.log(numb);
+
+const num = 3;
+console.log(num);
+
+const simbol = 'abc';
+console.log(simbol);
+
+const simb = 'bcd';
+console.log(simb);
+
+const sim = 'cde';
+console.log(sim);
+
+
+const list = [1, 2, 3, 4, 5];
+console.log(list);
+
+const list1 = [6, 7, 8, 9, 10];
+console.log(list1);
+
+const list2 = [11, 22, 33, 44, 55];
+console.log(list2);
+
+const text = ['labas', 'rytas', 'vakaras', 'diena', 'naktis'];
+console.log(text);
+
+const text1 = ['Jonas', 'Petras', 'Jurgis', 'Antanas', 'Ona'];
+console.log(text1);
+
+const text2 = ['saule', 'menulis', 'zvaigzde', 'dangus', 'vaivorykste'];
+console.log(text2);
+
+console.log('-----------------------------------------------------------');
+
+const suma = number + numb + num;
+console.log(suma);
+
+const zodis = 'mano';
+const zodis2 = 'vardas';
+const vardas = 'Arunas';
+
+const suma1 = `Sveiki, ${zodis} ${zodis2} ${vardas}.`;
+
+console.log(suma1);
