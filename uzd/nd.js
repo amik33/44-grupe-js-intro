a19c5f6aa5f22360f7b68ed2c3a94ab7fa43d231
Arūnas Mikalajūnas <mikalajunasar@gmail.com>
2023-05-10 15:36:11 +0300

    nd

---
+++ uzd/nd.js
@@ -0,0 +1,25 @@
+console.clear();
+
+function raides(tekstas, kintamieji) {
+    const funkcijos = {
+        'first': (tekstas) => tekstas[0],
+        'last': (tekstas) => tekstas[-1],
+        'mid': (tekstas) => tekstas[Math.round(tekstas.length/2)],
+        'size': (tekstas) => tekstas.length,
+        'lowercase': (tekstas) => tekstas.toLowerCase(),
+        'uppercase': (tekstas) => tekstas.toUpperCase(),
+        'uppercaseCount': (tekstas) => tekstas.uppercaseCount,
+        'lowercaseCount': (tekstas) => tekstas.lowercaseCount,
+    };
+
+    return funkcijos[kintamieji](tekstas);
+}
+
+console.log(raides('labas', 'first'));
+console.log(raides('labas', 'last'));
+console.log(raides('labas', 'mid'));
+console.log(raides('labas', 'size'));
+console.log(raides('labas', 'lowercase'));
+console.log(raides('labas', 'uppercase'));
+console.log(raides('labas', 'uppercaseCount'));
+console.log(raides('labas', 'lowercaseCount'));
